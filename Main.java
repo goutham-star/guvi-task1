896f208beef090c7a4f1f5bc8d580062e90c6f9c
goutham-star <gouthamaero29@gmail.com>
2025-10-23 15:08:06 +0530

    added main.java file

---
+++ Main.java
@@ -0,0 +1,117 @@
+
+public class Main {
+    // import java.util.Scanner;
+
+// public class Main{
+
+//1) question
+
+
+    // public static void main(String[] args) {
+    //     for( int i = 10 ; i <= 50; i++){
+
+    //     System.out.println(i);
+    //     }
+    // }
+
+
+
+
+// 2) question
+
+//   public static void main(String[] args) {
+        
+// Scanner obj = new Scanner(System.in);
+// System.out.println("Enter a number");
+
+// int number = obj.nextInt();
+
+// int reversed = 0;
+
+// while (number !=0) {
+//     int digit = number % 10;
+//     reversed = reversed * 10 +digit;
+//     number = number / 10 ;
+// }
+// System.out.println("Reversed number: " + reversed);
+
+// obj.close();
+//     }
+
+// 3)question
+
+//  public static void main(String[] args) {
+        
+//         int number = 123;
+
+//         if (number>100) {
+//             System.out.println("positive");
+//         } else {
+//             System.out.println("negative");
+//         }
+//     }
+
+// 4)question
+
+//  public static void main(String[] args) {
+        
+//         Scanner obj = new Scanner(System.in);
+
+//         int num1 = obj.nextInt();
+//         int num2 = obj.nextInt();
+//         int num3 = obj.nextInt();
+
+//      int smallest;
+
+//      if (num1 <=num2 && num1 <= num3) {
+//         smallest = num1;
+//      }else if( num2<= num1 && num2<= num3){
+//      smallest = num2;
+//      }
+//      else{
+//         smallest = num3;
+//      }
+//      System.out.println("smallest number is " + smallest);
+        
+//      obj.close();
+//      }
+
+// 5)question
+
+
+//  public static void main(String[] args) {
+//         Scanner obj = new Scanner(System.in);
+//      double amount = obj.nextDouble();
+//      double discountpercent = obj.nextDouble();
+//      double discount = (amount * discountpercent) / 100;
+//      double finalamount = amount - discount;
+
+//      System.out.println("purchase amount: "+ amount);
+//      System.out.println("Discount (" + discountpercent + "%):"+ discount);
+//      System.out.println("Final payable amount: " + finalamount);
+
+//      obj.close();
+//     }
+
+
+// 6)question
+
+//   public static void main(String[] args) {
+//         int n= 5;
+
+//         for(int i = 1; i<=n; i++){
+//             int num =5;
+//             for(int j =1; j<=n; j++){
+//                 if (j < i) {
+//                     System.out.print(j);
+//                 } else {
+//                    System.out.print(num); 
+//                 }
+//             }
+//           System.out.println();
+//         }
+//     }
+// }
+
+
+}
